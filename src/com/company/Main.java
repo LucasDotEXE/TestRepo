e99a92dee6d7db8b36e14b33c83165e476983ce3
Lucas Prins <lljPrins@avans.nl>
2019-02-04 13:35:30 +0100

    goodbye to a world lyrics

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is a nice world");
+
+
+        System.out.println("Thank you");
+        System.out.println("I'll say goodbye to");
+        System.out.println("Tho its the end of the world");
     }
 }

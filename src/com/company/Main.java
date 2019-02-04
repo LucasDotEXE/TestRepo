f6ecf0b6e99be419bb8d806d5a44047ea979dfd6
Lucas Prins <lljPrins@avans.nl>
2019-02-04 13:37:41 +0100

    Added credits

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,5 +12,6 @@ public class Main {
         System.out.println("Thank you");
         System.out.println("I'll say goodbye to");
         System.out.println("Tho its the end of the world");
+        System.out.println("Lyrics: goodbye to a world, by porter Robinson");
     }
 }
